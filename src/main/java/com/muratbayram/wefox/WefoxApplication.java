8e040e88572494486f85c1f53929960147a76032
Murat Bayram <murat.bayram@live.com>
2021-07-06 22:21:49 +0300

    project initialization

---
+++ src/main/java/com/muratbayram/wefox/WefoxApplication.java
@@ -0,0 +1,13 @@
+package com.muratbayram.wefox;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class WefoxApplication {
+
+    public static void main(String[] args) {
+        SpringApplication.run(WefoxApplication.class, args);
+    }
+
+}
---
+++ src/test/java/com/muratbayram/wefox/WefoxApplicationTests.java
@@ -0,0 +1,13 @@
+package com.muratbayram.wefox;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class WefoxApplicationTests {
+
+    @Test
+    void contextLoads() {
+    }
+
+}
