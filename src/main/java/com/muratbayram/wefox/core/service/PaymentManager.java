f3576827a9bef8d458e1fb1eb08afed49a86061a
Murat Bayram <murat.bayram@live.com>
2021-07-08 22:12:56 +0300

    parameterize endpoints

--- src/main/java/com/muratbayram/wefox/core/service/PaymentManager.java
+++ src/main/java/com/muratbayram/wefox/core/service/PaymentManager.java
@@ -4,20 +4,34 @@ import com.muratbayram.wefox.core.model.PaymentDTO;
 import com.muratbayram.wefox.core.model.PaymentErrorDTO;
 import com.muratbayram.wefox.core.port.PaymentDBPort;
 import org.springframework.beans.factory.annotation.Autowired;
+import org.springframework.beans.factory.annotation.Value;
 import org.springframework.stereotype.Service;
 import org.springframework.web.reactive.function.client.WebClient;
 
+import javax.annotation.PostConstruct;
 import java.util.function.Consumer;
 
 @Service
 public class PaymentManager {
 
-    private WebClient webClient = WebClient.create("http://localhost:9000");
+    @Value("${wefox.endpoint.payment}")
+    private String paymentEndpoint;
+    @Value("${wefox.endpoint.error-log}")
+    private String logEndpoint;
+
+    private WebClient paymentCheckClient;
+    private WebClient errorLogClient;
     private PaymentDBPort paymentDB;
 
     @Autowired
     public PaymentManager(PaymentDBPort paymentDB) {
         this.paymentDB = paymentDB;
+    }
+
+    @PostConstruct
+    private void init(){
+        paymentCheckClient = WebClient.create("http://" + paymentEndpoint);
+        errorLogClient = WebClient.create("http://" + logEndpoint);
     }
 
     public void handle(PaymentDTO paymentDTO) {
@@ -33,7 +47,7 @@ public class PaymentManager {
     }
 
     private void handleOnline(PaymentDTO paymentDTO){
-        webClient.post().uri("payment")
+        paymentCheckClient.post().uri("payment")
                 .bodyValue(paymentDTO)
                 .retrieve()
                 .toBodilessEntity()
@@ -56,7 +70,7 @@ public class PaymentManager {
         errorDTO.setErrorType(type);
         errorDTO.setErrorDescription(message);
 
-        webClient.post().uri("log")
+        errorLogClient.post().uri("log")
                 .bodyValue(errorDTO)
                 .retrieve()
                 .toBodilessEntity().subscribe();
